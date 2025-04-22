0eab476089626d93f9c494180ed7616b929511de
Prajaktabhosale306 <44079769+Prajaktabhosale306@users.noreply.github.com>
2025-04-22 22:09:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,8 @@ import pandas as pd
 # Ensure app modules are importable
 sys.path.append(os.path.join(os.path.dirname(__file__), "app"))
 
-from swagger_loader import load_swagger_from_url
-from test_generator import generate_test_cases
+from app.swagger_loader import load_swagger_from_url
+from app.test_generator import generate_test_cases
 from app.utils import extract_required_fields
 
 # Streamlit page configuration

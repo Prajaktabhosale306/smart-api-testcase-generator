d8dd1872c16fe83badff869bb42ecee50b2e740f
Prajaktabhosale306 <44079769+Prajaktabhosale306@users.noreply.github.com>
2025-04-22 13:48:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,9 @@ import json
 # Add 'app' directory to the Python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'app')))
 
-from swagger_loader import load_swagger_from_url
-from test_generator import generate_test_cases
-from utils import save_test_cases_to_json, save_test_cases_to_csv
+from app.swagger_loader import load_swagger_from_url
+from app.test_generator import generate_test_cases
+from app.utils import save_test_cases_to_json, save_test_cases_to_csv
 
 st.set_page_config(page_title="Smart API Test Case Generator", layout="wide")
 

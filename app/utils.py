bf5861879b5e54a7b1d6d147178c4509e2a76f19
Prajaktabhosale306 <44079769+Prajaktabhosale306@users.noreply.github.com>
2025-04-22 12:07:08 +0530

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,1 +1,9 @@
+def extract_required_fields(parameters):
+    required_fields = []
+    payload = {}
+    for param in parameters:
+        if param.get("required") and param.get("in") in ["query", "body"]:
+            required_fields.append(param["name"])
+            payload[param["name"]] = f"sample_{param['name']}"
+    return required_fields, payload
 

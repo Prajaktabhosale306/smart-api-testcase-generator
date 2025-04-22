b81607a2bd37a26e91c11324abbd7b6ccc530721
Prajaktabhosale306 <44079769+Prajaktabhosale306@users.noreply.github.com>
2025-04-22 12:06:40 +0530

    Update swagger_loader.py

--- app/swagger_loader.py
+++ app/swagger_loader.py
@@ -1,1 +1,11 @@
+import requests
+
+def load_swagger_from_url(swagger_url):
+    try:
+        response = requests.get(swagger_url)
+        response.raise_for_status()
+        return response.json()
+    except Exception as e:
+        print(f"[ERROR] Failed to load Swagger file: {e}")
+        return None
 

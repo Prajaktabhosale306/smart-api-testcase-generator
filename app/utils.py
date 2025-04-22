c08d48832422dcaf5137624026f4688ce0867b30
Prajaktabhosale306 <44079769+Prajaktabhosale306@users.noreply.github.com>
2025-04-22 20:47:00 +0530

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,9 +1,19 @@
-def extract_required_fields(parameters):
-    required_fields = []
-    payload = {}
-    for param in parameters:
-        if param.get("required") and param.get("in") in ["query", "body"]:
-            required_fields.append(param["name"])
-            payload[param["name"]] = f"sample_{param['name']}"
-    return required_fields, payload
+# app/utils.py
 
+import json
+import csv
+
+def save_test_cases_to_json(test_cases, filename="generated_test_cases.json"):
+    with open(filename, "w") as f:
+        json.dump(test_cases, f, indent=2)
+    print(f"[INFO] Test cases saved to {filename}")
+
+def save_test_cases_to_csv(test_cases, filename="generated_test_cases.csv"):
+    if not test_cases:
+        return
+    keys = test_cases[0].keys()
+    with open(filename, mode='w', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=keys)
+        writer.writeheader()
+        writer.writerows(test_cases)
+    print(f"[INFO] Test cases saved to {filename}")

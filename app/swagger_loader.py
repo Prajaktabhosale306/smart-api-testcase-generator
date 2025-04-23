f259118acdcd48836d58c4c4630c2cf03c5c1bc4
Prajaktabhosale306 <44079769+Prajaktabhosale306@users.noreply.github.com>
2025-04-23 19:56:01 +0530

    Update swagger_loader.py

--- app/swagger_loader.py
+++ app/swagger_loader.py
@@ -2,5 +2,11 @@ import requests
 
 def load_swagger_from_url(url):
     response = requests.get(url)
-    response.raise_for_status()  # Ensure we get a valid response
+    response.raise_for_status()
     return response.json()
+
+def extract_request_body(path_data):
+    try:
+        return path_data['requestBody']['content']['application/json']['schema']
+    except KeyError:
+        return {}

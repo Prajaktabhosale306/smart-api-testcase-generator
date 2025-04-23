504706393db5fccf17b64ead11075d9109830709
Prajaktabhosale306 <44079769+Prajaktabhosale306@users.noreply.github.com>
2025-04-23 09:24:57 +0530

    Update swagger_loader.py

--- app/swagger_loader.py
+++ app/swagger_loader.py
@@ -1,21 +1,12 @@
-# app/swagger_loader.py
-
 import requests
 
 def load_swagger_from_url(url):
-    """ Load Swagger data from a URL """
     response = requests.get(url)
+    response.raise_for_status()
     return response.json()
 
-def extract_request_body(swagger_data):
-    """ Extract request body from Swagger data """
-    request_bodies = {}
-    
-    # Loop through all paths in the Swagger spec to find request bodies
-    paths = swagger_data.get("paths", {})
-    for path, methods in paths.items():
-        for method, method_info in methods.items():
-            if "requestBody" in method_info:
-                request_bodies[path] = method_info["requestBody"]
-    
-    return request_bodies
+def extract_request_body(path_data):
+    try:
+        return path_data['requestBody']['content']['application/json']['schema']
+    except KeyError:
+        return {}

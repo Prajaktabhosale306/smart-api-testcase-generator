e7d116f9437a5ae2447b8a68f713322694d85519
Prajaktabhosale306 <44079769+Prajaktabhosale306@users.noreply.github.com>
2025-04-23 09:51:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import os
 import streamlit as st
 
 # Add 'app' directory to Python path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "app")))
+sys.path.append(os.path.join(os.path.dirname(__file__), "app"))
 
 from swagger_loader import load_swagger_from_url, extract_request_body
 from test_generator import generate_test_cases
